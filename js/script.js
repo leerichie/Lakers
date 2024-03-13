d6eaca6a238934a95d97d442e194efc486bcfa62
leerichie <leerichie@wp.pl>
2024-03-13 18:20:29 +0100

    commit new layout

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// // Place this JavaScript code in a separate script.js file
+
+// window.addEventListener('load', function() {
+//   var introAnimation = document.getElementById('intro-animation');
+//   var delayInSeconds = 2000; // Set the delay in milliseconds (e.g., 2000 = 2 seconds)
+
+//   setTimeout(function() {
+//     introAnimation.style.display = 'none';
+//     // Show the main page or perform any necessary actions after the animation
+//   }, delayInSeconds);
+// });
